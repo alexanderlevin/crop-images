d23d380f5fd24e203ecefdc84cc4c5088b7c97bd
Alex Levin <levin@mit.edu>
2014-11-17 00:35:13 -0800

    Simple tracking of coordinates
    Using Jcrop demos

--- js/main.js
+++ js/main.js
@@ -25,11 +25,40 @@ $.get('/js/templates/selector.html', function(result) {
     setImage(indexedPictures[newValue].path);
   });
 
-  jQuery(function($) {
-    $('#picture').Jcrop();
+  jQuery(function($){
+
+    var jcrop_api;
+
+    $('#picture').Jcrop({
+      onChange:   showCoords,
+      onSelect:   showCoords,
+      onRelease:  clearCoords
+    },function(){
+      jcrop_api = this;
+    });
   });
 });
+
 
 var setImage = function(path) {
     $('#picture').attr('src', path);
 }
+
+
+// Simple event handler, called from onChange and onSelect
+// event handlers, as per the Jcrop invocation above
+function showCoords(c)
+{
+  $('#x1').val(c.x);
+  $('#y1').val(c.y);
+  $('#x2').val(c.x2);
+  $('#y2').val(c.y2);
+  $('#w').val(c.w);
+  $('#h').val(c.h);
+};
+
+function clearCoords()
+{
+  $('#coords input').val('');
+};
+
